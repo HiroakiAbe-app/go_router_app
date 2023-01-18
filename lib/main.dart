bb11cdff572e6a273d6a87aea530b2ba2c03478a
HiroakiAbe-app <hiroaki.abe@p.radiko.co.jp>
2023-01-18 18:04:10 +0900

    ページ作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:go_router_sample/first_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final _router = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const MyHomePage(
-      title: 'go_router_sample',
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MyHomePage(
+        title: 'go_router_sample',
+      ),
     ),
-  ),
-]);
+    GoRoute(
+      path: '/first',
+      pageBuilder: (context, state) => CustomTransitionPage(
+        child: const FirstPage(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+      ),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,48 +41,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Navigation Sample'),
+            onTap: () => context.push('/first'),
+          ),
+          ListTile(
+            title: Text('Redirect Sample'),
+            onTap: () => context.go('/redirect'),
+          )
+        ],
       ),
     );
   }
